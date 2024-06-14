6e8bca6496ce29db998f420b0dff4ae3c7b23d52
Suyog Luitel <suyogluitel235@gmail.com>
2024-06-14 15:42:00 +0545

    upto footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Header from './container/Header/Header'
 import AboutUs from './container/AboutUs/AboutUs'
 import SpecialMenu from './container/Menu/SpecialMenu'
 import Chef from './container/Chef/Chef'
+import Intro from './container/Intro/Intro'
+import Laurels from './container/Laurels/Laurels'
+import Gallery from './container/Gallery/Gallery'
+import FindUs from './container/FindUs/FindUs'
+import Footer from './container/Footer/Footer'
 
 const App = () => {
   return (
@@ -14,6 +19,11 @@ const App = () => {
       <AboutUs />
       <SpecialMenu />
       <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   )
 }
