1f7ee9555ab791488041909d4774809a9ace5550
Suyog Luitel <suyogluitel235@gmail.com>
2024-06-14 14:43:44 +0545

    about and menu section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Header from './container/Header/Header'
+import AboutUs from './container/AboutUs/AboutUs'
+import SpecialMenu from './container/Menu/SpecialMenu'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <AboutUs />
+      <SpecialMenu />
     </div>
   )
 }
